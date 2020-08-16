47a9df95c775bae9ef6645302d5881e631a448b4
dementr80 <dementr80@gmail.com>
2020-08-16 09:27:44 +0500

    The Header component was changed 	1. Resizable font was added

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
+import styles from './Header.scss';
 
 import GlobeIcon from './globe.svg';
 
@@ -7,8 +8,8 @@ class Header extends Component {
   render() {
     return (
             <nav className="navbar navbar-expand-lg navbar-dark" style={{backgroundColor: "#000000"}}>
-              <Link className="navbar-brand m-0 p-0 ml-4 mr-3 h1" to='/'>Takeshi<br/>Kitano</Link>
-              
+              <Link className={["navbar-brand", "m-0", "p-0", "ml-4", "mr-3", styles.customNavbarBrand].join(" ")}
+                    to='/'>Takeshi<br/>Kitano</Link>
               <button className="navbar-toggler" type="button" 
                       data-toggle="collapse" 
                       data-target="#navbarSupportedContent" 
@@ -20,18 +21,18 @@ class Header extends Component {
               <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav">
                   <li className="nav-item active">
-                    <Link className="nav-link" to='/Movies'>Movies</Link>
+                    <Link className={["nav-link", styles.customNavLink].join(" ")} to='/Movies'>Movies</Link>
                   </li>
                   <li className="nav-item active">
-                    <Link className="nav-link" to='/Biography'>Biography</Link>
+                    <Link className={["nav-link", styles.customNavLink].join(" ")} to='/Biography'>Biography</Link>
                   </li>
                 </ul>
                 <ul className="navbar-nav ml-auto mr-4">
                   <li className="nav-item active mr-2" style={{color: "#ffffff"}}>
-                    <a className="nav-link">Light</a>
+                    <a className={["nav-link", styles.customNavLink].join(" ")}>Light</a>
                   </li>
-                  <li className="nav-item">
-                    <img src={GlobeIcon} width="30" height="30" className="" alt="GlobeIcon"></img>
+                  <li className="nav-item mt-1">
+                    <img src={GlobeIcon} width="30" height="30" alt="GlobeIcon"></img>
                   </li>
                 </ul>
               </div>
